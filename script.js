0dd241bd365e6f1e8a4954c7575545402dbb0da6
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-04-11 14:33:17 +0600

    Add hamburger menu

---
+++ script.js
@@ -0,0 +1,6 @@
+const ham = document.getElementById('ham-menu');
+const menu = document.getElementById('menu');
+function clickMenu() {
+  ham.style.display = 'block';
+  menu.style.display = 'none';
+}

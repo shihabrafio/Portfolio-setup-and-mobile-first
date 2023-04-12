4b860343309a5581032a445198ff2dac4344ce5b
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-04-12 23:15:49 +0600

    Add error message

--- script.js
+++ script.js
@@ -12,11 +12,23 @@ const form = document.querySelector('form');
 const msg = document.querySelector('textarea[name="user_message"]');
 const error = document.getElementById('error');
 const name = document.querySelector('input[name="user_name"]');
-const email = document.querySelector('input[name="email"]');s
+const email = document.querySelector('input[name="email"]');
 
 form.addEventListener('submit', (event) => {
   const errormsg = [];
   if (name.value.trim() === '') {
     errormsg.push('Name field is required');
+  } else if (email.value.trim() === '') {
+    errormsg.push('Email field is required');
+  } else if (msg.value.trim() === '') {
+    errormsg.push('Message field is required');
+  } else if (email.value !== email.value.toLowerCase()) {
+    errormsg.push('\u00A0\u00A0Email must be in lowercase\u00A0\u00A0');
+  }
+  if (errormsg.length > 0) {
+    event.preventDefault();
+    error.textContent = errormsg.join('. ');
+  } else {
+    error.textContent = '';
   }
 });

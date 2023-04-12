e8883f711e28ca7983eb5f89dc9d6e14fda3dca5
shihabrafio <aimshihabuddinkhan@gmail.com>
2023-04-12 22:28:14 +0600

    Add error file

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ function clickX() {
   ham.style.display = 'none';
   menu.style.display = 'block';
 }
+const form = document.querySelector('form');
+const msg = document.querySelector('textarea[name="user_message"]');
+const error = document.getElementById('error');
+const name = document.querySelector('input[name="user_name"]');
+const email = document.querySelector('input[name="email"]');s
+
+form.addEventListener('submit', (event) => {
+  const errormsg = [];
+  if (name.value.trim() === '') {
+    errormsg.push('Name field is required');
+  }
+});
